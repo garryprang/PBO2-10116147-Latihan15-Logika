b1d3646e47437244c49f051a9181d28639010534
grryv <grryv@grry>
2020-10-21 19:50:36 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan15/logika/PBO210116147Latihan15Logika.java
@@ -0,0 +1,27 @@
+package pbo2.pkg10116147.latihan15.logika;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk mengimplementasikan operator logika.
+ */
+
+public class PBO210116147Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
